9cbcbd5293d66cbe6a03b9ca3c761475c1e92bec
am-barbosa <alexandre.barbosa@chalmers.se>
2026-02-05 14:53:38 +0100

    Update remind.py for testing

--- remind.py
+++ remind.py
@@ -5,7 +5,7 @@ import pandas as pd
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-REMINDER_DAYS = 10
+REMINDER_DAYS = 0 # testing
 
 slack = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
